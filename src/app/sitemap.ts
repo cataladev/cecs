9590798246bdc6335377ecb30163bb80875c2e9a
Carlos <catalacarlos23@gmail.com>
2025-05-12 19:03:37 -0400

    made work with current domain

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: "https://www.catala.dev",
+            url: "https://www.cecsknights.org",
             lastModified: new Date(),
             priority: 1,
         },
--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -6,7 +6,7 @@ import { FaDiscord } from 'react-icons/fa';
 export default function Buttons() {
   const buttons = [
     {
-      href: 'https://discord.gg/sWCNrnFxq5',
+      href: 'https://discord.cecsknights.org',
       label: 'Join Discord',
       className: 'bg-[#5865F2] hover:bg-[#4752C4]',
       icon: <FaDiscord className="w-5 h-5 mr-2" />,
